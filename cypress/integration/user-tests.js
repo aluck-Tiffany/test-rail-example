924ce22857242408dfcd88066c8f3edd3ed9a3d9
t.ZHANG <tiffanyzhang2015@foxmail.com>
2022-04-06 21:03:32 +1000

    c110to 13 and 2=1+0

--- cypress/integration/user-tests.js
+++ cypress/integration/user-tests.js
@@ -1,12 +1,13 @@
 /// <reference types="cypress" />
 
 describe('User tests', () => {
-  it('C111 User can log in', () => {
+  it('C14 User can log in', () => {
     cy.wait(1000)
   })
 
-  it('C112 User can log out', () => {
-    cy.log('in the last test!')
-    cy.wait(1000)
+  it('C15 User can log out', () => {
+    // cy.log('in the last test!')
+    // cy.wait(1000)
+    expect(2).to.equal(1+0)
   })
 })
