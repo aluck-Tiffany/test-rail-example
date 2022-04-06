aa47c06d413532ca01193162875e10f6535e2bc0
t.ZHANG <tiffanyzhang2015@foxmail.com>
2022-04-06 21:15:49 +1000

    2eqls1plus1

--- cypress/integration/user-tests.js
+++ cypress/integration/user-tests.js
@@ -1,5 +1,7 @@
 /// <reference types="cypress" />
 
+const { expect } = require("chai")
+
 describe('User tests', () => {
   it('C14 User can log in', () => {
     cy.wait(1000)
@@ -8,6 +10,7 @@ describe('User tests', () => {
   it('C15 User can log out', () => {
     cy.log('in the last test!')
     cy.wait(1000)
+    expect(2).equal.to(1+1)
     
   })
 })
