16c50609e01ec5ea124a4ddc1a2e0f73c2fc198f
t.ZHANG <tiffanyzhang2015@foxmail.com>
2022-04-06 17:41:26 +1000

    c110 to C13,111 to 14 and 112to15

--- cypress/integration/public-site.js
+++ cypress/integration/public-site.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 
 describe('Public site', () => {
-  it('C1 Everyone can view the public site', () => {
+  it('C13 Everyone can view the public site', () => {
     cy.wait(10000)
   })
 })
--- cypress/integration/user-tests.js
+++ cypress/integration/user-tests.js
@@ -1,11 +1,11 @@
 /// <reference types="cypress" />
 
 describe('User tests', () => {
-  it('C2 User can log in', () => {
+  it('C14 User can log in', () => {
     cy.wait(1000)
   })
 
-  it('C3 User can log out', () => {
+  it('C15 User can log out', () => {
     cy.log('in the last test!')
     cy.wait(1000)
   })
