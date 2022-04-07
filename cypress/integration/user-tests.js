bf5db71b76691743ccb6179bd46c0850e68251ac
t.ZHANG <tiffanyzhang2015@foxmail.com>
2022-04-07 13:58:03 +1000

    2eql1+0/1

--- cypress/integration/user-tests.js
+++ cypress/integration/user-tests.js
@@ -5,11 +5,12 @@
 describe('User tests', () => {
   it('C14 User can log in', () => {
     cy.wait(1000)
+    expect(2).to.equal(1+1)
   })
 
   it('C15 User can log out', () => {
     cy.log('in the last test!')
     cy.wait(1000)
-    expect(2).to.equal(1+1)
+    expect(2).to.equal(1+0)
   })
 })
