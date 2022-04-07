536f5e1b66aeab0131538ef75891ca284ef133c6
t.ZHANG <tiffanyzhang2015@foxmail.com>
2022-04-08 09:54:18 +1000

    trueFalse

--- cypress/integration/user-tests.js
+++ cypress/integration/user-tests.js
@@ -5,12 +5,12 @@ describe('User tests', () => {
     cy.wait(1000)
     expect(2).to.equal(1+1)
     expect(true).to.equal(true)
-
   })
 
   it('C15 User can log out', () => {
     cy.log('in the last test!')
     cy.wait(1000)
     expect(2).to.equal(1+0)
+    expect(true).to.equal(false)
   })
 })
