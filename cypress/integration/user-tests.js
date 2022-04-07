3014e7e009a09cfc746753f15f58f4e6dd87fe42
t.ZHANG <tiffanyzhang2015@foxmail.com>
2022-04-07 10:33:03 +1000

    addCy

--- cypress/integration/user-tests.js
+++ cypress/integration/user-tests.js
@@ -10,6 +10,6 @@ describe('User tests', () => {
   it('C15 User can log out', () => {
     cy.log('in the last test!')
     cy.wait(1000)
-    expect({ name: 'Jane' }).to.eql({ name: 'Jane' })
+    cy.expect({ name: 'Jane' }).to.eql({ name: 'Jane' })
   })
 })
